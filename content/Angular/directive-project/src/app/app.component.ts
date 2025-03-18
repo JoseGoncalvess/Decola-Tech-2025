8d10fbb297ecc0f17a8ef1e207917a0246a86c2e
JoseGoncalvess <joseinacio774@gmail.com>
2025-03-18 14:23:51 -0300

    [Learn] Ngmodel and ng Style

--- content/Angular/directive-project/src/app/app.component.ts
+++ content/Angular/directive-project/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./card/card.component";
 import { CommonModule } from '@angular/common';
+import { CompAtributosComponent } from "./comp-atributos/comp-atributos.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardComponent, CommonModule],
+  imports: [RouterOutlet, CardComponent, CommonModule, CompAtributosComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] // Corrigido aqui
 })
---
+++ content/Angular/directive-project/src/app/comp-atributos/comp-atributos.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms"
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-comp-atributos',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './comp-atributos.component.html',
+  styleUrl: './comp-atributos.component.css'
+})
+export class CompAtributosComponent {
+  style: String = "enable";
+  fundo: String = "purple";
+  letras: String = "white";
+
+  item: string = "";
+
+  lista: string[] = []
+
+
+  constructor() {
+    this.lista.push(this.item);
+  }
+
+  mudou() {
+    if (this.style == "desable") {
+      this.style = "enable";
+    } else {
+      this.style = "desable";
+    }
+  }
+
+  colorir() {
+    if (this.fundo == "purple") {
+      this.fundo = "green";
+      this.letras = "black";
+    } else {
+      this.fundo = "purple";
+      this.letras = "white";
+    }
+  }
+
+  adicionarLista(): void {
+    if (this.item != "") {
+      this.lista.push(this.item);
+    }
+  }
+
+
+
+}
+
