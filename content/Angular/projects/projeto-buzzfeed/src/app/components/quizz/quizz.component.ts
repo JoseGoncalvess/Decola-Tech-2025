1c188e5c41e3697dfd2d3d3372b74a5f3c3b0155
José Gonçalves <joseinacio774@gmail.com>
2025-04-02 23:04:23 -0300

    [Buuzfed] Estructure Finished

--- content/Angular/projects/projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ content/Angular/projects/projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 @Component({
   selector: 'app-quizz',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  title :string = ""
+  questions: any
+  questionSelect :any
+
+  answrs :string[] =[]
+  answrSelected :string = ""
+
+  questionIndex : number = 0
+  questionMaxIndex : number = 0
+
+  finished: boolean = false
+
+constructor() {}
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished = false
+
+      this.title = quizz_questions.title
+      this.questions =quizz_questions.questions
+      this.questionSelect = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+
+
+playerChoose(optionSelect:string){
+  this.answrs.push(optionSelect)
+  this.advancedQuestion()
+}
+
+ private advancedQuestion(){
+  this.questionIndex++
+  if (this.questionMaxIndex > this.questionIndex ) {
+    this.questionSelect = this.questions[ this.questionIndex]
+  }else{
+    const responseFinal : string = this.checkResult(this.answrs)
+  this.finished = true
+  // AQUI O ANNY NÂO INDENTIFICA O COMPORTAMENTO da "responseFinal" PRCISANOD ESPLICITAR OCM O KEYOF
+  this.questionSelect = quizz_questions.results[responseFinal as keyof typeof quizz_questions.results]
+  }
+
+}
+
+private checkResult( answrsNow:string[]){
+const result = answrsNow.reduce((previousValue,currentValue,currentIndex, array)=>{
+  if (array.filter(item => item === previousValue).length >
+  array.filter(item => item === currentValue).length) {
+    return previousValue
+  }
+  else{
+    return currentValue
+  }
+})
+
+return result
+}
 
 }
