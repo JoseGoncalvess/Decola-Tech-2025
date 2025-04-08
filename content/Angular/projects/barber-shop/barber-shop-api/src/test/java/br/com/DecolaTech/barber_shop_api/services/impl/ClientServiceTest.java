b6dce3fe0e17d535df08cd73b1bfa0dc87bbb3da
JoseGoncalvess <joseinacio774@gmail.com>
2025-04-08 19:14:33 -0300

    [TEST] SERVICE

--- content/Angular/projects/barber-shop/barber-shop-api/src/test/java/br/com/DecolaTech/barber_shop_api/services/impl/ClientServiceTest.java
+++ content/Angular/projects/barber-shop/barber-shop-api/src/test/java/br/com/DecolaTech/barber_shop_api/services/impl/ClientServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.DecolaTech.barber_shop_api.services.impl;
 
 import br.com.DecolaTech.barber_shop_api.exception.EmailInUseException;
+import br.com.DecolaTech.barber_shop_api.exception.NotFoundException;
 import br.com.DecolaTech.barber_shop_api.exception.PhoneInUseException;
 import br.com.DecolaTech.barber_shop_api.models.entity.ClientEntity;
 import br.com.DecolaTech.barber_shop_api.repository.ClientEntityRepository;
@@ -14,12 +15,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.util.Assert;
 
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -29,10 +34,12 @@ class ClientServiceTest {
 
     @Mock()
     private static ClientEntityRepository repository ;
+    @Mock()
+    private static IClientQueryService queryService;
 
 
     @InjectMocks
-    private  ClientQueryService queryService;
+    private  ClientService clientService;
 
     @BeforeEach
     void setUp() {
@@ -44,36 +51,28 @@ class ClientServiceTest {
     void saveToGreat() {
 
         when(repository.existsByEmail(cleint.getEmail())).thenReturn(false);
-        when(queryService.verifyEmail(cleint.getEmail())).thenReturn(true);
-
         when(repository.existsByPhone(cleint.getPhone())).thenReturn(false);
-        when(queryService.verifyPhone(cleint.getPhone())).thenReturn(true);
 
-
-        when(repository.save(cleint)).thenReturn(cleint);
-
-
-        ClientEntity newClient = repository.save(cleint);
+        ClientEntity newClient = clientService.save(cleint);
 
         assertEquals(newClient.getName(),"João");
         assertEquals(newClient.getPhone(),"87991959595");
         assertEquals(newClient.getId(),1L);
         assertEquals(newClient.getSchedules(), Set.of());
     }
+
+
     @Test
     void saveToEmailInUseException() {
         String message = "O e-mail " + cleint.getEmail() + " já está em uso";
         when(repository.existsByEmail(cleint.getEmail())).thenReturn(true);
 
-        // Verificação da exceção esperada
         EmailInUseException exception = assertThrows(EmailInUseException.class, () -> {
             queryService.verifyEmail(cleint.getEmail());
         });
 
-        // Validar a mensagem da exceção, se necessário
         assertEquals(message, exception.getMessage());
 
-        // Verificar interações no mock
         verify(repository).existsByEmail(cleint.getEmail());
 
     }
@@ -98,12 +97,61 @@ class ClientServiceTest {
 
 
     @Test
-    void update() {
+    void updateOfGreat() {
+        ClientEntity newcleint = new ClientEntity(12L,"Rebeka","testando@gmail.com","89991958476", Set.of());
+        when(repository.findById(anyLong())).thenReturn(Optional.of(cleint));
+        when(queryService.findById(anyLong())).thenReturn(cleint);
+        when(clientService.update(newcleint)).thenReturn(newcleint);
+
+        ClientEntity clinetUpdate =  clientService.update(newcleint);
+
+        assertEquals(clinetUpdate.getName(),"Rebeka");
+        assertEquals(clinetUpdate.getPhone(),"89991958476");
+        assertEquals(clinetUpdate.getEmail(),"testando@gmail.com");
+        assertEquals(clinetUpdate.getId(),12L);
+
+    }
+
+
+
+    @Test
+    void deleteOfGreat() {
+        when(queryService.findById(cleint.getId())).thenReturn(cleint);
+
+        clientService.delete(cleint.getId());
+
+        verify(repository).deleteById(cleint.getId());
+        verify(queryService).findById(cleint.getId());
+
     }
 
     @Test
-    void delete() {
+    void deleteOfException() {
+        when( queryService.verifyEmail(cleint.getEmail())).thenReturn(true);
+        when(queryService.findById(cleint.getId())).thenReturn(cleint);
+
+        clientService.delete(cleint.getId());
+
+        verify(repository).deleteById(cleint.getId());
+        verify(queryService).findById(cleint.getId());
+
     }
+
+
+
+
+    @Test
+    void deleteorException() {
+        when(queryService.findById(cleint.getId())).thenThrow(new  EmailInUseException("O e-mail " + cleint.getEmail() + " já está em uso"));
+
+        EmailInUseException exception   = assertThrows(EmailInUseException.class, () -> {
+            queryService.findById(cleint.getId());});
+
+        verify(queryService).findById(cleint.getId());
+        assertEquals(exception.getMessage(), "O e-mail " + cleint.getEmail() + " já está em uso");
+
+    }
+
 
 
 
